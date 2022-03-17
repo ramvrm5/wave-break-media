2fe4673ecfa5afdad1efce15e6ee51a13692102b
ram verma <42866242+ramvrm5@users.noreply.github.com>
2022-03-17 19:22:30 +0530

    basic test

---
+++ src/_test_/imageList.test.js
@@ -0,0 +1,22 @@
+/* import React from 'react';
+import ReactDOM from 'react-dom';
+import ImageList from '../components/ImageList';
+
+import { render } from '@testing-library/react'; */
+
+describe('Testing sum', () => {
+    function sum(a, b) {
+       return a + b;
+    }
+
+    it('should equal 4',()=>{
+       expect(sum(2,2)).toBe(4);
+      })
+
+    test('also should equal 4', () => {
+        expect(sum(2,2)).toBe(4);
+      }) 
+});
+
+
+
